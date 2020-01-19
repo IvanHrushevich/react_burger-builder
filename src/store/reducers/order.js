a38d3803e8e23441304611d8deabcbc3cfef0217
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-01-19 16:35:09 +0300

    Perform refactoring of reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   orders: [],
@@ -9,49 +10,27 @@ const initialState = {
 export const orderReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
-        purchased: false
-      };
+      return updateObject(state, { purchased: false });
 
     case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
-        loading: true
-      };
+    case actionTypes.FETCH_ORDERS_START:
+      return updateObject(state, { loading: true });
 
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = {
-        ...action.orderData,
-        id: action.orderId
-      };
+      const newOrder = updateObject(action.orderData, { id: action.orderId });
 
-      return {
-        ...state,
+      return updateObject(state, {
         loading: false,
         orders: [...state.orders, newOrder],
         purchased: true
-      };
+      });
 
     case actionTypes.PURCHASE_BURGER_FAIL:
     case actionTypes.FETCH_ORDERS_FAIL:
-      return {
-        ...state,
-        loading: false
-      };
-
-    case actionTypes.FETCH_ORDERS_START:
-      return {
-        ...state,
-        loading: true
-      };
+      return updateObject(state, { loading: false });
 
     case actionTypes.FETCH_ORDERS_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        orders: action.orders
-      };
+      return updateObject(state, { loading: false, orders: action.orders });
 
     default:
       return state;
